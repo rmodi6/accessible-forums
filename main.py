3828e00a5a8cf0128edb79d197896c0a204af706
Ruchit Modi <modi.ruchit6@gmail.com>
2020-11-03 12:24:58 -0600

    Updated code to read all csv files within data directory and its subdirectories

--- main.py
+++ main.py
@@ -16,8 +16,14 @@ cli.register(app)
 def load_db():
     """Load existing data into database."""
     click.echo('Loading data into database...')
-    path = os.path.join(app.config['BASE_DIR'], "data/**/*.csv")
-    for file_name in tqdm(glob.glob(path)):
+    file_names = []
+    root = os.path.join(app.config['BASE_DIR'], "data")
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            file_name = os.path.join(path, name)
+            if file_name.endswith(".csv"):
+                file_names.append(file_name)
+    for file_name in tqdm(file_names):
         load_users_and_posts(file_name)
         load_threads(file_name)
     click.echo('Data loaded successfully')
