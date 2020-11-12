397c2551dcd85664ea06449b4f01c81110c97fc2
Ruchit Modi <modi.ruchit6@gmail.com>
2020-11-12 15:33:51 -0600

    Improved db-load using batch commit

--- app/db_utils.py
+++ app/db_utils.py
@@ -5,31 +5,38 @@ from tqdm import tqdm
 from app import db
 from app.models import User, Post, Thread
 
+BATCH_SIZE = 100
+
 
 def load_data_from_file(file_name):
     index = 0
     with open(file_name, 'r', encoding="utf8") as f:
+        user_ids, post_ids, thread_ids = set(), set(), set()
         for row in tqdm(csv.reader(f)):
             try:
                 if index > 0:
-                    existing_user = User.query.filter_by(username=row[2]).first()
-                    if existing_user is None:
-                        user = User(id=row[8], username=row[2], email='abc' + str(index) + '@xyz.com')
-                        user.set_password('abc12345')
+                    thread, date_time, author, post, parent_posts, post_id, parent_id, thread_id, author_id, origin_id, label = row
+                    if author not in user_ids:
+                        user = User(id=author_id, username=author, email='abc@xyz.com')
+                        user.set_password('abc123')
                         db.session.add(user)
-                        db.session.commit()
-                    existing_post = Post.query.filter_by(id=row[5]).first()
-                    if existing_post is None:
-                        post = Post(id=row[5], body=row[3], user_id=row[8], thread_id=row[7], parent_ids=row[6],
-                                    label=row[10])
+                        user_ids.add(author)
+                    if post_id not in post_ids:
+                        post = Post(id=post_id, body=post, user_id=author_id, thread_id=thread_id, parent_ids=parent_id,
+                                    label=label)
                         db.session.add(post)
-                        db.session.commit()
-                    existing_thread = Thread.query.filter_by(id=row[7]).first()
-                    if existing_thread is None:
+                        post_ids.add(post_id)
+                    if thread_id not in thread_ids:
                         thread = Thread(id=row[7], title=row[0])
                         db.session.add(thread)
+                        thread_ids.add(thread_id)
+
+                    if index % BATCH_SIZE == 0:
                         db.session.commit()
+
             except Exception as e:
                 print(e)
             finally:
                 index += 1
+
+        db.session.commit()
