e399aff9017fb1ab11c00b3febfd02cf1de48f33
Ruchit Modi <modi.ruchit6@gmail.com>
2020-11-23 12:14:25 -0600

    Disable elasticsearch on startup if server is not running

--- app/__init__.py
+++ app/__init__.py
@@ -37,6 +37,8 @@ def create_app(config_class=Config):
     babel.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
+    if app.elasticsearch and not app.elasticsearch.ping():
+        app.elasticsearch = None
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
